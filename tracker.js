4afe92ec950a2843e6289f6b98b84ec45f04f266
dpsmnsk <dpsmnsk@gmail.com>
2022-03-02 22:25:25 +0500

    connect messaging

--- tracker.js
+++ tracker.js
@@ -1,53 +1,75 @@
 'use strict';
 
 const dgram = require('dgram');
-// const Buffer = require('buffer').Buffer;
-// const urlParse = require('url').parse;
 
-module.exports.getPeers = (torrent, callback) => {
-  const socket = dgram.createSocket('udp4');
-  const url = torrent.announce.toString('utf8');
-
-  // 1. send connect request
-  udpSend(socket, buildConnReq(), url);
-
-  socket.on('message', response => {
-    if (respType(response) === 'connect') {
-      // 2. receive and parse connect response
-      const connResp = parseConnResp(response);
-      // 3. send announce request
-      const announceReq = buildAnnounceReq(connResp.connectionId);
-      udpSend(socket, announceReq, url);
-    } else if (respType(response) === 'announce') {
-      // 4. parse announce response
-      const announceResp = parseAnnounceResp(response);
-      // 5. pass peers to callback
-      callback(announceResp.peers);
-    }
-  });
-};
-
-function udpSend(socket, message, rawUrl, callback=()=>{}) {
-  const url = new URL(rawUrl);
-  socket.send(message, 0, message.length, url.port, url.host, callback);
-}
-
-function respType(resp) {
-  // ...
-}
+const crypto = require('crypto');
 
 function buildConnReq() {
-  // ...
+    const buf = Buffer.alloc(16);
+
+    // connection id
+    buf.writeUInt32BE(0x417, 0);
+    buf.writeUInt32BE(0x27101980, 4);
+    // action
+    buf.writeUInt32BE(0, 8);
+    // transaction id
+    crypto.randomBytes(4).copy(buf, 12);
+
+    return buf;
 }
 
 function parseConnResp(resp) {
-  // ...
+    return {
+        action: resp.readUInt32BE(0),
+        transactionId: resp.readUInt32BE(4),
+        connectionId: resp.slice(8)
+    }
+}
+
+module.exports.getPeers = (torrent, callback) => {
+    const socket = dgram.createSocket('udp4');
+    const url = torrent.announce.toString('utf8');
+
+    // 1. send connect request
+    udpSend(socket, buildConnReq(), url);
+
+    socket.on('message', response => {
+        if (respType(response) === 'connect') {
+            // 2. receive and parse connect response
+            const connResp = parseConnResp(response);
+            // 3. send announce request
+            const announceReq = buildAnnounceReq(connResp.connectionId);
+            udpSend(socket, announceReq, url);
+        } else if (respType(response) === 'announce') {
+            // 4. parse announce response
+            const announceResp = parseAnnounceResp(response);
+            // 5. pass peers to callback
+            callback(announceResp.peers);
+        }
+    });
+};
+
+function udpSend(socket, message, rawUrl, callback = () => { }) {
+    const url = new URL(rawUrl);
+    socket.send(message, 0, message.length, url.port, url.host, callback);
+}
+
+function respType(resp) {
+    // ...
+}
+
+function buildConnReq() {
+    // ...
+}
+
+function parseConnResp(resp) {
+    // ...
 }
 
 function buildAnnounceReq(connId) {
-  // ...
+    // ...
 }
 
 function parseAnnounceResp(resp) {
-  // ...
+    // ...
 }
